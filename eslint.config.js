3368cb6d1636de8f837d442b34ce7db1c55d4a8b
Andrew Molyuk <andrew@molyuk.com>
2025-09-01 22:10:00 +0300

    chore: remove eslint-plugin-eslint-plugin from configuration and package files

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,11 @@
 import js from '@eslint/js'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
-import eslintPlugin from 'eslint-plugin-eslint-plugin'
 
 export default [
   // ignore common build and dependency folders
   { ignores: ['node_modules/**', 'dist/**', '.git/**', 'examples/**', '**/*.d.ts'] },
   js.configs.recommended,
   eslintPluginPrettierRecommended,
-  {
-    files: ['src/**/*.js'],
-    ...eslintPlugin.configs.recommended,
-  },
   {
     files: ['**/*.js'],
     // default language options and globals for plugin source files (Node environment)
